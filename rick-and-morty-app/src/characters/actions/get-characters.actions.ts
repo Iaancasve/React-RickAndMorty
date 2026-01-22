02b022a9928f8add07faa5416a6dfc862aeb3d4e
Ian <iaancastellanos1234@gmail.com>
2026-01-22 21:13:08 +0100

    Api config, interfaces and getCharacters

---
+++ rick-and-morty-app/src/characters/actions/get-characters.actions.ts
@@ -0,0 +1,22 @@
+import { rickMortyApi } from '../api/rickmorty.api';
+import type { Character } from '../interfaces/character.interface';
+
+export const getCharactersAction = async (name: string = '', status: string = ''): Promise<Character[]> => {
+    
+    const response = await rickMortyApi.get('/character', {
+        params: {
+            name: name,
+            status: status
+        }
+    });
+
+    // Mapeamos los datos para limpiar la respuesta
+    return response.data.results.map((char: any) => ({
+        id: char.id,
+        name: char.name,
+        status: char.status,
+        species: char.species,
+        image: char.image,
+        location: char.location
+    }));
+};
